370bbe67f0bfb835dd2b3e2f8f91b86528478644
roshan <roshankajrolkar6@gmail.com>
2024-01-05 13:05:12 +0530

    carousel component added in home page

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Header from "../components/header/Header";
 import Routers from "../routes/Routes";
-import  Carousel  from "../components/header/commonComponent/Carousel";
 
 const Layout = () => {
   return (
     <>
       <Header />
       <Routers />
-      <Carousel />
     </>
   );
 };
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,12 @@
 import SearchBy from "../../components/searchby";
 import Search from "../../components/searchbar/Search";
+import Carousel from "../../components/header/commonComponent/Carousel";
 const Home = () => {
   return (
     <div>
       <SearchBy />
       <Search />
+      <Carousel />
     </div>
   );
 };
