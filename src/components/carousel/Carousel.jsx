d8817709392b5eadc755c869e597a06d35320c3b
roshan <roshankajrolkar6@gmail.com>
2024-01-05 17:10:51 +0530

    carousel added

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,30 +1,55 @@
-import React, { useEffect,useState } from "react";
+import React from "react";
 import { cardData } from "../../utils/globalvariables/GlobalPath";
 import { Card } from "../card/Card";
 import styles from "./Carousel.module.css"
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FreeMode} from 'swiper/modules';
+import 'swiper/css/free-mode';
+import 'swiper/css';
 
 const Carousel = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  useEffect(() => {
-    const interval = setInterval(() => {
-      // Move to the next item cyclically
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % cardData.length);
-    }, 3000);
-
-    // Clear the interval on component unmount
-    return () => clearInterval(interval);
-  }, [cardData.length]);
   return (
-    <div>
+    <div className={styles.carouselContainer}>
       <div className={styles.carouselHeader}>
         <div className={styles.carouselDeal}>Hot deals</div>
         <div className={styles.carouselViewAll}>View all</div>
       </div>
-    <div className={styles.carousel} >
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={7/2}
+      rewind={true}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+      freeMode={true}
+      modules={[FreeMode]}
+      breakpoints={{
+        0:{
+          slidesPerView: 3/2,
+          spaceBetween: 10,
+        },
+        640: {
+          slidesPerView: 5/2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 7/2,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 7/2,
+          spaceBetween: 50,
+        },
+      }}
+    >
+      <div className={styles.carousel} >
       {
-        cardData.map((data,index) =><Card data={data}  key={index} currentIndex={currentIndex}/>)
+        cardData.map((data,index) =><SwiperSlide><Card data={data}  key={index}/><SwiperSlide/></SwiperSlide>)
       }
     </div>
+      ...
+    </Swiper>
+ 
+    
     </div>
   )
 }
