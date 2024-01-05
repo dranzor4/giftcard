8da6f870821b150ef2afa9f6e05b49a660a51cb6
roshan <roshankajrolkar6@gmail.com>
2024-01-05 12:22:09 +0530

    card added

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../components/header/Header";
 import Routers from "../routes/Routes";
+import  Carousel  from "../components/header/commonComponent/Carousel";
 
 const Layout = () => {
   return (
     <>
       <Header />
       <Routers />
+      <Carousel />
     </>
   );
 };
