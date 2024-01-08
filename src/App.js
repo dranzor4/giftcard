e1d7c5853707dc49f3de0478fab8fc842c02d754
aashadsolkar <aashadsolkar96@gmail.com>
2024-01-08 12:07:52 +0530

    static header done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,4 @@
-// import React, { useState } from 'react'
-// import appService from './service/AppService'
-// const App = () => {
-//   const [a, setA] = useState(10)
-// const UpdateHandler=()=>{
-//   let b =  appService.UpdateAppService(a);
-//   setA(b);
-// }
-
-//   return (
-//     <div>{a}
-//     <button onClick={UpdateHandler}>Click</button></div>
-//   )
-// }
-
-// export default App
-
 import { BrowserRouter as Router } from 'react-router-dom';
-
 import Layout from "./layout/Layout";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
