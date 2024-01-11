0323a228bcff62da60a73ccd414da52b1e709fb4
roshan <roshankajrolkar6@gmail.com>
2024-01-11 17:44:47 +0530

    bug resolved

--- src/components/Button/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import styles from "./Button.module.css"
-const Button = ({children,className,handleClick}) => {
-  return (
-    <button className={`${styles.defaultButton} ${className}`} onClick={handleClick}>{children}</button>
-  )
-}
-
-export default Button
