d3be059bd6c1a4e4f6396dbee1a1fd9aa81b9540
WebiknowsIT <64890186+WebiknowsIT@users.noreply.github.com>
2024-01-11 19:09:54 +0530

    my gift card

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Header from "../components/header";
+import Header from "../components/Header";
 import Routers from "../routes/Routes";
 import DownBarLogin from "../components/DownBarLogin";
 import Sidebar from "../components/Sidebar";
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -4,8 +4,9 @@ import {Route, Routes } from 'react-router-dom';
 import Home from '../pages/home';
 import Login from '../pages/login';
 import ProductDetail from '../components/productDetail/ProductDetail';
-import MyProfile from '../pages/myprofile';
+import MyProfile from '../pages/mygiftcard';
 import CalculatorModal from '../components/Calculator';
+import MyGiftCard from '../pages/mygiftcard';
 
 
 
@@ -17,6 +18,7 @@ const Routers = () => {
                 <Route exact path="/productDetail/:productIndex" element={<ProductDetail/>}/>
                 <Route exact path="/my-profile" element={<MyProfile/> }/>
                 <Route path="/calculator" element={<CalculatorModal/> }/>
+                <Route path="/my-gift-card" element={<MyGiftCard/> }/>
             </Routes>
         
     );
