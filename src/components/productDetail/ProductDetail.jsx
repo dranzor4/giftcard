1c9ca34202cc5afdbcfef0dc8930fa3ac17027a9
aashadsolkar <aashadsolkar96@gmail.com>
2024-01-13 00:48:54 +0530

    fixes done

--- src/components/productDetail/ProductDetail.jsx
+++ src/components/productDetail/ProductDetail.jsx
@@ -6,7 +6,12 @@ import GiftForSomeoneFrom from "../GiftForSomeoneFrom";
 import Offers from "../Offers";
 import Redeemable from "../Redeemable";
 import { useNavigate } from "react-router-dom";
-
+import CustomInput from "../CustomInput";
+import NetEffecttive from "../NetEffecttive";
+import { Form } from "react-bootstrap";
+import Button from "../Button";
+import TermsAndCondition from "../TermsAndCondition";
+import { Swiper, SwiperSlide } from "swiper/react";
 const ProductDetail = () => {
   // const { productIndex } = useParams();
   // const navigate=useNavigate();
@@ -24,6 +29,8 @@ const ProductDetail = () => {
   const navigate = useNavigate()
 
   const [selectedOption, setSelectedOption] = useState('');
+  const [sendNowOrLater, setSendNowOrLater] = useState('');
+
   const handleOptionChange = (event) => {
     setSelectedOption(event.target.value);
   };
@@ -31,7 +38,58 @@ const ProductDetail = () => {
     { value: 'Buy for Self', label: 'Buy for Self' },
     { value: 'Gift to Someone', label: 'Gift to Someone' },
   ];
+  let PriceRadioBtnArry = [
+    {
+      value: "250",
+      label: "₹250"
+    },
+    {
+      value: "500",
+      label: "₹250"
+    },
+    {
+      value: "1000",
+      label: "₹250"
+    },
+    {
+      value: "5000",
+      label: "₹250"
+    }
+  ]
+  let sendNowOrLaterBtnArr = [
+    {
+      value: "sendnow",
+      label: "Send Now"
+    },
+    {
+      value: "sendlater",
+      label: "Send Later"
+    }
+  ]
+  let giftCategoryArr = [
+    {
+        value: "best_wishes",
+        label: "Best Wishes"
+    },
+    {
+        value: "cccasions",
+        label: "Occasions"
+    },
+    {
+        value: "festival",
+        label: "Festival"
+    },
+    {
+        value: "anniversary",
+        label: "Anniversary"
+    }
+]
+  const handlesetSendNowOrLaterChange = (event) => {
+    setSendNowOrLater(event.target.value);
+  };
 
+  const PriceRadioBtns = () => PriceRadioBtnArry.map((obj, index) => <RadioBox key={index} onOptionChange={handleOptionChange} selectedOption={selectedOption} options={[{ value: obj.value, label: obj.label }]} className={"byforself-pric"} />)
+  const SendNowOrLaterRadioBtns = () => sendNowOrLaterBtnArr.map((obj, index) => <RadioBox key={index} onOptionChange={handlesetSendNowOrLaterChange} selectedOption={sendNowOrLater} options={[{ value: obj.value, label: obj.label }]} className={"byforself-pric"} />)
   return (
     <>
       <div className={styles.mainConatiner}>
@@ -53,21 +111,122 @@ const ProductDetail = () => {
             <GiftForSomeoneFrom />
           </div> */}
           <div className="row">
-            <div className="col-md-3 bg-primary">1</div>
-            <div className="col-md-9 bg-warning">
+            <div className="col-md-6">
+              <CustomCard label={true} tag={true} />
+              <Redeemable />
+              <Offers />
+            </div>
+            <div className={`${styles.productForm} | col-md-6`}>
               <div className={`row`}>
                 <div className={`${styles.gridForm} row`}>
-                  <div className="form-toggle p-5">buttons</div>
-                  <div className="fomr-1 p-5">Form 1</div>
-                  <div className={styles.form2}>FOrm 2</div>
+                  <div className="form-toggle ">
+                    <div className="text-center line-height-0">
+                      <RadioBox options={options} selectedOption={selectedOption} onOptionChange={handleOptionChange} className="product-selforgift-btn" />
+                    </div>
+                  </div>
+                  <div className="fomr-1">
+                    <div className='form-price-section'>
+                      <label className='form-label p-b-8'>
+                        Select Gift Value
+                      </label>
+                      <div className='radiobtn-group'>
+                        <PriceRadioBtns />
+                      </div>
+                      <label className='form-label'>Or</label>
+
+                      <CustomInput label={"Enter Amount"} hintText={"Min: ₹100, Max: ₹1,00,000"} hintShow={true} />
+
+                      <CustomInput label={"Quantity"} hintText={"Min: 1, Max: 10"} hintShow={true} />
+                    </div>
+                  </div>
+                  <div className={styles.form2}>
+                    <NetEffecttive />
+                  </div>
                 </div>
-                <div className="row bg bg-danger p-5">
-                  form 3
+                <div className="row">
+                  <div className='form-delivery-details-section '>
+                    <div className='form-divider'>
+                      <h3><span>Delivery Details</span></h3>
+                    </div>
+
+                    <div className="row">
+                      <div className='form-sms-or-mail-section col-md-6'>
+                        <label className='form-label'>How would you like to send it?</label>
+                        <div className='checkbox-group'>
+                          <Form.Check
+                            type={"checkbox"}
+                            id={`default-checkbox`}
+                            label={`Email`}
+                          />
+                          <Form.Check
+                            type={"checkbox"}
+                            id={`default-checkbox`}
+                            label={`SMS`}
+                          />
+                        </div>
+                        <p className='form-light-text'>Gift card will be delivered to this Mobile number and Email ID <span><img src="/Images/whatsapp-icon.svg" alt="" /></span></p>
+                        <label className='form-label'>How would you like to send it?</label>
+                        <SendNowOrLaterRadioBtns />
+                      </div>
+                      <div className='form-user-details col-md-6'>
+                        <label className='form-label'>Who is this gift for?</label>
+                        <p className='form-light-text'>Gift card will be delivered to this Mobile number and Email ID</p>
+                        <CustomInput label={"First Name"} />
+                        <CustomInput label={"last Name"} />
+                        <CustomInput label={"Email ID"} />
+                        <CustomInput label={"Mobile Number"} />
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className="row bg-success float-right p-5">form 2</div>
+          <div className="row col-md-9  float-end">
+          <div className='form-divider'>
+                <h3><span>Personalise your Gift Card</span></h3>
+              </div>
+            <div className="col-md-6"></div>
+            <div className='form-personalise-section col-md-6 '>
+              <label className='form-label'>
+                Choose Category
+              </label>
+              <div className='radiobtn-group'>
+                <Swiper
+                  slidesPerView={3.2}
+                  spaceBetween={16}
+                  loop={true}
+                >
+                  {
+                    giftCategoryArr.map((ele) => <SwiperSlide><RadioBox onOptionChange={handleOptionChange} selectedOption={selectedOption} options={[{ value: ele.value, label: ele.label }]} className={"byforself-pric"} /></SwiperSlide>)
+                  }
+
+                </Swiper>
+              </div>
+              <label className='form-label'>
+                Select a Theme
+              </label>
+              <div className='radiobtn-group gift'>
+                <Swiper
+                  slidesPerView={3.1}
+                  spaceBetween={10}
+                  loop={true}
+                >
+                  <SwiperSlide><div className='each-slide-container'><CustomCard /></div></SwiperSlide>
+                  <SwiperSlide><div className='each-slide-container'><CustomCard /></div></SwiperSlide>
+                  <SwiperSlide><div className='each-slide-container'><CustomCard /></div></SwiperSlide>
+                  <SwiperSlide><div className='each-slide-container'><CustomCard /></div></SwiperSlide>
+                </Swiper>
+              </div>
+              <CustomInput label={"Email Subject"} hintText={"Max 24 characters"} hintShow={true} />
+              <CustomInput type='textarea' label={"Personal Message"} hintText={"Min: ₹100, Max: ₹1,00,000"} hintShow={true} />
+              <div className='form-btn'>
+                <Button>Preview eGift Card</Button>
+              </div>
+              <TermsAndCondition />
+            </div>
+
+          </div>
         </div>
       </div>
     </>
