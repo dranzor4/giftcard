526e0355d2c7c5588b7df3e173c127696498b712
roshan <roshankajrolkar6@gmail.com>
2024-01-18 14:42:17 +0530

    gift card header and same class card name issue fixed

--- src/components/CustomCard/index.jsx
+++ src/components/CustomCard/index.jsx
@@ -1,21 +1,28 @@
-import React from 'react'
-import "./CustomCard.css"
+import React from "react";
+import "./CustomCard.css";
 
-const CustomCard = ({ label, tag=false }) => {
+const CustomCard = ({ label, tag = false }) => {
   return (
-    <div className="card">
-      <div className="cardHeader">
+    <div className="card1">
+      <div className="cardHeader1">
         <img src="/Images/amazon-card.png" alt="card" />
-        {tag && <div className="cardtag"><p>3%</p><p>OFF</p></div>}
+        {tag && (
+          <div className="cardtag">
+            <p>3%</p>
+            <p>OFF</p>
+          </div>
+        )}
       </div>
-      {
-        label && <div className="cardBody">
+      {label && (
+        <div className="cardBody">
           <p className="cardBoldText">Amazon Shopping Voucher</p>
-          <p className="cardLightText">Validity: 1 year from the date of issue</p>
+          <p className="cardLightText">
+            Validity: 1 year from the date of issue
+          </p>
         </div>
-      }
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default CustomCard
+export default CustomCard;
