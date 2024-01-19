ed0509efc41114c219c9016e419606275cceabba
roshan <roshankajrolkar6@gmail.com>
2024-01-19 11:06:43 +0530

    converted module css to normal css

--- src/components/DownBarLogin/index.jsx
+++ src/components/DownBarLogin/index.jsx
@@ -1,24 +1,31 @@
 // import React, { useState } from 'react'
-import Style from "./DownBarLogin.module.css"
-import Button from '../Button'
-import TransparentOverlay from "../TransparentOverlay/index"
+import "./DownBarLogin.css";
+import Button from "../Button";
+import TransparentOverlay from "../TransparentOverlay/index";
 const DownBarLogin = ({ toggleDownbar, state }) => {
-    return (<>
-        <div className={`${Style.downbar} ${state ? Style.active : ""} `}>
-            <div className={Style.DownBarheader} onClick={toggleDownbar}>
-                <img src="/Images/rectangle-bar.svg" alt="" />
-            </div>
-            <div className={Style.DownBarContent}>
-                <p>Are you sure you want to log out?</p>
-                <div className={Style.LogoutBtnGrp}>
-                    <Button handleClick={toggleDownbar} className={Style.logoutCancelBtn}>Cancel</Button>
-                    <Button className={Style.logoutBtn}>Logout</Button>
-                </div>
-            </div>
+  return (
+    <>
+      <div className={`downbar ${state ? "active" : ""} `}>
+        <div className="DownBarheader" onClick={toggleDownbar}>
+          <img src="/Images/rectangle-bar.svg" alt="" />
         </div>
-        <TransparentOverlay handleClick={toggleDownbar} customeClass={Style.downbarLoginOverlay} isOpen={state} />
+        <div className="DownBarContent">
+          <p>Are you sure you want to log out?</p>
+          <div className="LogoutBtnGrp">
+            <Button handleClick={toggleDownbar} className="logoutCancelBtn">
+              Cancel
+            </Button>
+            <Button className="logoutBtn">Logout</Button>
+          </div>
+        </div>
+      </div>
+      <TransparentOverlay
+        handleClick={toggleDownbar}
+        customeClass="downbarLoginOverlay"
+        isOpen={state}
+      />
     </>
-    )
-}
+  );
+};
 
-export default DownBarLogin
+export default DownBarLogin;
--- src/components/Filter/Filter.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import styles from "./Filter.module.css"
-const Filter = () => {
-  return (
-    <div className={styles.filterContent}>Filter</div>
-  )
-}
-
-export default Filter
