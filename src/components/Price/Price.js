b94c705d159b6c3bef736df45a882a1d558f7e93
MukeshSuthar1403 <133212436+MukeshSuthar1403@users.noreply.github.com>
2024-01-22 10:33:38 +0530

    changes file and add folders

---
+++ src/components/Price/Price.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Price.css";
+
+const Price = () => {
+    return (
+        <div>
+            <span className="priceSymbol">
+                ₹
+            </span>
+        </div>
+    )
+}
+
+export default Price
