69b9bce3295c677309fd9baf7fb10497934ee96f
Uzairfaquih <saniainamdar2522@gmail.com>
2024-01-23 12:40:27 +0530

    change cart to react bootstrap

--- src/pages/cart/index.jsx
+++ src/pages/cart/index.jsx
@@ -1,66 +1,70 @@
-import arrowDown from "../../assests/images/icon/down-arrow.svg";
+import React from "react";
+import { Container, Row, Col, Image, Button, Form } from "react-bootstrap";
+import arrowDown from "../../assets/images/icon/down-arrow.svg";
 import "./Cart.css";
+
 const Cart = () => {
   const handleCartAcc = () => {
     console.log("yo");
   };
+
   return (
     <>
-      <div className="container cart-item">
-
-        <div className="row m-0">
-          <div className="col-5 p-0">
-            <img src="Images/amazon-card.png" alt="" className="img-fluid" />
-          </div>
-
-          <div className="col-7 cart-data">
+      <Container className="cart-item">
+        <Row>
+          <Col xs={5} className="p-0">
+            <Image src="Images/amazon-card.png" alt="" fluid />
+          </Col>
+          <Col xs={7} className="cart-data">
             <p className="h14">Amazon Shopping Voucher</p>
             <p className="h16">₹1000</p>
             <p className="h12">Value of Reward Points: ₹150</p>
-          </div>
-        </div>
-          
-        <div className="row mx-0 mt-3">
-         
-          <div className="col-5 p-0">
+          </Col>
+        </Row>
+
+        <Row className="mt-3">
+          <Col xs={5} className="p-0">
             <p className="h12 text-primary">
               View more details
               <i className="cart-icon">
-                <img src={arrowDown} alt="" onClick={handleCartAcc} />
+                <Image src={arrowDown} alt="" onClick={handleCartAcc} />
               </i>
             </p>
-          </div>
-
-          <div className="col-7 text-center">
-            <div className="row">
-              <div className="col-5 cart-select">
-                <label htmlFor="cart-qty">Qty:</label>
-                <select name="" id="cart-qty" className="h12">
-                  <option value="">1</option>
-                  <option value="">1</option>
-                  <option value="">1</option>
-                </select>
-              </div>
-              <div className="col-7 cart-add-del pt-2">
+          </Col>
+          <Col xs={7} className="text-center">
+            <Row>
+              <Col xs={5} className="cart-select">
+                <Form.Group controlId="cart-qty">
+                  <Form.Label>Qty:</Form.Label>
+                  <Form.Control as="select" className="h12">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                  </Form.Control>
+                </Form.Group>
+              </Col>
+              <Col xs={7} className="cart-add-del pt-2">
                 <span className="btn-edit px-1">Edit</span>
                 <span className="px-1">Delete</span>
-              </div>
-            </div>
-          </div>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
 
-        </div>
-      </div>
-
-      <div className="row mx-0 my-2">
-        <div className="col-5">
+      <Row className="my-2">
+        <Col xs={5}>
           <p className="h12">Sub total</p>
           <p className="h18">₹2000</p>
-        </div>
-        <div className="col-7 px-2 cart-btn">
-        <button type="button" className="btn btn-primary w-100 h-100">Proceed to Buy</button> 
-        </div>
-      </div>
+        </Col>
+        <Col xs={7} className="px-2 cart-btn">
+          <Button variant="primary" className="w-100 h-100">
+            Proceed to Buy
+          </Button>
+        </Col>
+      </Row>
     </>
   );
 };
+
 export default Cart;
