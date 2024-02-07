5da195fba343da5f106e027fa0694d43676e73cf
aashadsolkar <aashadsolkar96@gmail.com>
2024-02-08 01:17:16 +0530

    bootstra changes done

--- src/components/HeaderCompnents/HeaderSearchBar/Index.jsx
+++ src/components/HeaderCompnents/HeaderSearchBar/Index.jsx
@@ -4,8 +4,8 @@ import "./SearchBar.css";
 import useIsMobile from "../../../customHooks/useIsMobile";
 
 const HeaderSearchBar = ({ customStyle }) => {
-  const [searchData ,setSearchData] = useState([])
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchData, setSearchData] = useState([])
+  const [searchIterm, setSearchIterm] = useState("");
   const [loading, setLoading] = useState(false);
   const searchArr = [
     {
@@ -109,72 +109,68 @@ const HeaderSearchBar = ({ customStyle }) => {
       category: "Demo"
     }
   ]
-  const handleSearch = (event)=> {
-    const searchTerm = event.target.value;
-    const words = searchTerm.split('');
-    if(words.length === 0)  setSearchData([])
-    if(words.length >= 3){
+  const handleSearch = (event) => {
+    const searchIterm = event.target.value;
+    const words = searchIterm.split('');
+    if (words.length === 0) setSearchData([])
+    if (words.length >= 3) {
       setLoading(true)
       setTimeout(() => {
-        const searchFilter = searchArr.filter((item) => item.title.toLowerCase().includes(searchTerm));
-      setSearchData(searchFilter)
-      setLoading(false)
-      },1000)
-      
+        const searchFilter = searchArr.filter((item) => item.title.toLowerCase().includes(searchIterm));
+        setSearchData(searchFilter)
+        setLoading(false)
+      }, 1000)
     }
-    setSearchTerm(searchTerm)
+    setSearchIterm(searchIterm)
   }
 
   const handleSelect = (item) => {
     setSearchData([])
   }
-  const hanldeClear =() => {
-    setSearchTerm("")
+  const hanldeClear = () => {
+    setSearchIterm("")
     setSearchData([])
   }
-  
+
   const mobile = useIsMobile();
   return (
     <Form inline className={customStyle}>
-      <InputGroup className="searchBox">
-        {mobile && (
-          <InputGroup.Text className="searchMobleBtn" id="search-bar">
-            {loading ? (
-              <span class="loader"></span>
-            ) : (
-              <img src="/Images/Search.svg" alt="search-icon" />
-            )}
-          </InputGroup.Text>
-        )}
+      <InputGroup className="rounded-pill border border-1 border-secondary-subtle py-2 px-3">
+        <InputGroup.Text className="p-0 bg-transparent border-0 " id="search-bar">
+          {loading ? (
+            <span class="loader"></span>
+          ) : (
+            <img src="/Images/Search.svg" alt="search-icon" />
+          )}
+        </InputGroup.Text>
+
         <Form.Control
           placeholder="Search for Products or Brands"
-          aria-label="Username"
+          aria-label="username"
           aria-describedby="search-bar"
-          className="SearchInput"
+          className="p-0 ps-3 bg-transparent border-0 fs-12 outline-0 no-focus-outline"
           onChange={handleSearch}
-          value={searchTerm}
-        // onChange={device && onChange}
+          value={searchIterm}
         />
-        {searchTerm && ( // Render clear button only when there's a search term
-          <InputGroup.Text className="clearSearchBtn" onClick={hanldeClear}>
-            <img src="/Images/Close.svg" alt="clear-icon" />
+        {searchIterm && ( // Render clear button only when there's a search term
+          <InputGroup.Text className="p-0 bg-transparent border-0" onClick={hanldeClear}>
+            <img src="/Images/Close.svg" alt="clear-icon" width="12px" height="12px"/>
           </InputGroup.Text>
         )}
-        {!mobile && (
-          <InputGroup.Text className="searchMobleBtn" id="search-bar">
+        {/* todo for desktop view */}
+        {/* <InputGroup.Text className="searchMobleBtn" id="search-bar">
             <img src="/Images/Search.svg" alt="search-icon" />
-          </InputGroup.Text>
-        )}
+          </InputGroup.Text> */}
       </InputGroup>
 
       <Dropdown show={searchData.length > 0} >
-        <Dropdown.Menu className="search-dropdown">
+        <Dropdown.Menu className="w-100 overflow-auto p-0" style={{maxHeight: "500px"}}>
           {
             searchData.map((item) => {
               return (
-                <Dropdown.Item key={item.title} className="search-item"  onClick={() => handleSelect(item)}>
-                  <Row className="search-item-title"><p>{item.title}</p></Row>
-                  <Row className="search-item-category"><p>{item.category}</p></Row>
+                <Dropdown.Item key={item.title} className="border border-bottom-1 px-3 py-2" onClick={() => handleSelect(item)}>
+                  <Row className=" fs-14 fw-normal"><p>{item.title}</p></Row>
+                  <Row className="fs-12 fw-normal"><p>{item.category}</p></Row>
                 </Dropdown.Item >
               )
             })
